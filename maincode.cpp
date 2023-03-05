d16aa0b686e14942b70a6c0adc03f84fc6a33cc2
Riya Jain <riyajainchd@gmail.com>
2023-03-05 22:55:32 +0530

    reading DE's

--- maincode.cpp
+++ maincode.cpp
@@ -1,16 +1,19 @@
 #include "ds.h"
 
-int main(){
-    cout<<"code start"<<endl;
-    if(take_input_csv("clean_data.csv"))
-        cout<<"INPUT TAKEN"<<endl;
+int main()
+{
+    cout << "code start" << endl;
+    if (take_input_csv("clean_data.csv"))
+        cout << "INPUT TAKEN" << endl;
 
-    if(take_input_classroom("Classroom.csv"))
-        cout<<"CLASSROOMS READ" <<endl;
+    if (take_input_classroom("Classroom.csv"))
+        cout << "CLASSROOMS READ" << endl;
 
     allocate_ic(ICs);
     allocate_classroom_same(ICs);
     allocate_dc(D_core);
+    // Made changes here
+    allocate_dc(D_Elec);
     debug_print();
-    classroom_print();    
+    classroom_print();
 }
