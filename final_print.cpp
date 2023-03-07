50cd0f15ebcce9472014323cf4ba54e1767b9e1b
dakshigoel22 <2020uee0134@iitjammu.ac.in>
2023-03-07 18:23:58 +0530

    Output in CSV

--- final_print.cpp
+++ final_print.cpp
@@ -1,70 +1,156 @@
 #include "ds.h"
+#include <fstream>
+
+// void debug_print(){
+//     cout<<"PRINT CALLED"<<endl;
+//     for(int i=0; i< dept.size(); i++){
+//         cout<<"TABLE FOR "<<dept[i]->name<<endl;
+//         for(int j=0; j< 5; j++){
+//             for(int k=0; k< 11; k++){
+//                 cout<<dept[i]->table[j][k].first<<" ";
+//                 if( dept[i]->table[j][k].first == 0 ){
+//                     cout << "empty";
+//                     //fout<<"empty"<<",";
+//                 }
+//                 else{
+//                     cout<< dept[i]->table[j][k].second->course_code;
+//                     //fout<< "dept[i]->table[j][k].second->course_code"<<",";
+//                     int le = dept[i]->table[j][k].second->course_code.length();
+//                     if(dept[i]->table[j][k].first == 2){
+//                         cout<<"(TUT)";
+//                         //fout<<"(TUT)"<<",";
+//                         le += 5;
+//                     }
+//                     if(dept[i]->table[j][k].first == 3){
+//                         cout<<"(PRA)";
+//                         //fout<<"(PRA)"<<",";
+//                         le += 5;
+//                     }
+//                     for(int letters=0; letters< 16-le; letters++){
+//                         cout<<" ";
+//                     }
+//                 }
+//             }
+//             cout<<endl;
+//             //fout<<endl;
+//         }
+//     }
+//     cout << "COURSES AND CLASSROOMS ALLOCATED TO THEM:"<<endl;
+//     for(int i=0; i<ICs.size(); i++){
+//         cout<<ICs[i]->course_code<<"  -  ";
+//         for(int j=0; j<ICs[i]->allocated_classrooms.size(); j++){
+//             cout<<ICs[i]->allocated_classrooms[j]<<" / ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// void classroom_print(){
+//     cout<<"CLASSROOM INFO"<<endl;
+//     for(int i=0; i< room.size(); i++){
+//         cout<<"TABLE FOR "<<room[i]->id<<endl;
+//         for(int j=0; j< 5; j++){
+//             for(int k=0; k< 11; k++){
+//                 if( room[i]->class_table.table[j][k].first == 0 ){
+//                     cout << "empty";
+//                 }
+//                 else{
+//                     cout<< room[i]->class_table.table[j][k].second->course_code;
+//                     int le = room[i]->class_table.table[j][k].second->course_code.length();
+//                     if(room[i]->class_table.table[j][k].first == 2){
+//                         cout<<"(TUT)";
+//                         le += 5;
+//                     }
+//                     if(room[i]->class_table.table[j][k].first == 3){
+//                         cout<<"(PRA)";
+//                         le += 5;
+//                     }
+//                     for(int letters=0; letters< 16-le; letters++){
+//                         cout<<" ";
+//                     }
+//                 }
+//             }
+//             cout<<endl;
+//         }
+//     }
+// }
 
 void debug_print(){
-    cout<<"PRINT CALLED"<<endl;
+    ofstream fout("output.csv");
+
+    fout << "PRINT CALLED" << endl;
     for(int i=0; i< dept.size(); i++){
-        cout<<"TABLE FOR "<<dept[i]->name<<endl;
+        fout << "TABLE FOR " << dept[i]->name << endl;
         for(int j=0; j< 5; j++){
             for(int k=0; k< 11; k++){
-                // cout<<dept[i]->table[j][k].first<<" ";
+                fout << dept[i]->table[j][k].first << ",";
                 if( dept[i]->table[j][k].first == 0 ){
-                    cout << "empty           ";
+                    fout << "empty";
                 }
                 else{
-                    cout<< dept[i]->table[j][k].second->course_code;
+                    fout << dept[i]->table[j][k].second->course_code;
                     int le = dept[i]->table[j][k].second->course_code.length();
                     if(dept[i]->table[j][k].first == 2){
-                        cout<<"(TUT)";
+                        fout << "(TUT)";
                         le += 5;
                     }
                     if(dept[i]->table[j][k].first == 3){
-                        cout<<"(PRA)";
+                        fout << "(PRA)";
                         le += 5;
                     }
                     for(int letters=0; letters< 16-le; letters++){
-                        cout<<" ";
+                        fout << " ";
                     }
                 }
+                fout << ",";
             }
-            cout<<endl;
+            fout << endl;
         }
     }
-    cout << "COURSES AND CLASSROOMS ALLOCATED TO THEM:"<<endl;
+    fout << "COURSES AND CLASSROOMS ALLOCATED TO THEM:" << endl;
     for(int i=0; i<ICs.size(); i++){
-        cout<<ICs[i]->course_code<<"  -  ";
+        fout << ICs[i]->course_code << ",";
         for(int j=0; j<ICs[i]->allocated_classrooms.size(); j++){
-            cout<<ICs[i]->allocated_classrooms[j]<<" / ";
+            fout << ICs[i]->allocated_classrooms[j] << "/";
         }
-        cout<<endl;
+        fout << endl;
     }
+
+    fout.close();
 }
 
 void classroom_print(){
-    cout<<"CLASSROOM INFO"<<endl;
+    ofstream fout("classroom_info.csv");
+
+    fout << "CLASSROOM INFO" << endl;
     for(int i=0; i< room.size(); i++){
-        cout<<"TABLE FOR "<<room[i]->id<<endl;
+        fout << "TABLE FOR " << room[i]->id << endl;
         for(int j=0; j< 5; j++){
             for(int k=0; k< 11; k++){
                 if( room[i]->class_table.table[j][k].first == 0 ){
-                    cout << "empty           ";
+                    fout << "empty,";
                 }
                 else{
-                    cout<< room[i]->class_table.table[j][k].second->course_code;
+                    fout << room[i]->class_table.table[j][k].second->course_code;
                     int le = room[i]->class_table.table[j][k].second->course_code.length();
                     if(room[i]->class_table.table[j][k].first == 2){
-                        cout<<"(TUT)";
+                        fout << "(TUT)";
                         le += 5;
                     }
                     if(room[i]->class_table.table[j][k].first == 3){
-                        cout<<"(PRA)";
+                        fout << "(PRA)";
                         le += 5;
                     }
                     for(int letters=0; letters< 16-le; letters++){
-                        cout<<" ";
+                        fout << " ";
                     }
+                    fout << ",";
                 }
             }
-            cout<<endl;
+            fout << endl;
         }
     }
+
+    fout.close();
 }
+
